0c03c73bbdd984339f4cced413a6dec90341fbd3
Paul <paul_lodder@live.nl>
2020-06-25 15:57:47 +0200

    Make default delimiter tab

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-"use strict";
-var http = require("http"),
-  stream = require("stream"),
-  bodyParser = require("body-parser"),
-  pathUtils = require("path"),
-  express = require("express"),
-  Papa = require("papaparse"),
-  fs = require("fs"),
+'use strict'
+var http = require('http'),
+  stream = require('stream'),
+  bodyParser = require('body-parser'),
+  pathUtils = require('path'),
+  express = require('express'),
+  Papa = require('papaparse'),
+  fs = require('fs'),
   app = express(),
   PORT = process.env.PORT || 5000,
-  appDir = pathUtils.resolve(__dirname, "static");
+  appDir = pathUtils.resolve(__dirname, 'static')
 
-const { Readable } = require("stream");
+const { Readable } = require('stream')
 
-app.use(bodyParser.json());
-app.use(express.urlencoded());
+app.use(bodyParser.json())
+app.use(express.urlencoded())
 
-app.post("/download", function (req, res) {
-  var subset_rows = [];
-  fs.readFile(pathUtils.resolve(appDir, "data/data.csv"), function (err, data) {
-    var subset_rows = [];
-    var read_results = Papa.parse(data.toString("utf-8"), {
+app.post('/download', function (req, res) {
+  var subset_rows = []
+  fs.readFile(pathUtils.resolve(appDir, 'data/data.csv'), function (err, data) {
+    var subset_rows = []
+    var read_results = Papa.parse(data.toString('utf-8'), {
       dynamicTyping: true,
-    });
-    var all_rows = read_results["data"];
-    subset_rows.push(all_rows.shift()); // add headers
-    req.body.idxs.split("").forEach(function (el, ind) {
-      if (el == "1") {
-        subset_rows.push(all_rows[ind]);
+    })
+    var all_rows = read_results['data']
+    subset_rows.push(all_rows.shift()) // add headers
+    req.body.idxs.split('').forEach(function (el, ind) {
+      if (el == '1') {
+        subset_rows.push(all_rows[ind])
       }
-    });
-    res.contentType("text/plain");
-    res.set(
-      "Content-disposition",
-      'attachment; filename="dramaticpenguin.csv"'
-    );
-    res.send(Papa.unparse(subset_rows));
-  });
-});
-app.use(express.static("static"));
+    })
+    res.contentType('text/plain')
+    res.set('Content-disposition', 'attachment; filename="dramaticpenguin.csv"')
+    res.send(Papa.unparse(subset_rows, { delimiter: '\t' }))
+  })
+})
+app.use(express.static('static'))
 http.createServer(app).listen(PORT, function () {
-  console.log("Express server listening on port " + PORT);
-  console.log("http://localhost:" + PORT);
-});
+  console.log('Express server listening on port ' + PORT)
+  console.log('http://localhost:' + PORT)
+})
