c1891a210cb489f0c42ab5193572d3a058e87524
Paul <paul_lodder@live.nl>
2020-07-01 12:48:59 +0200

    Update app.js and convert file to download only subset

--- app.js
+++ app.js
@@ -17,22 +17,28 @@ app.use(express.urlencoded())
 
 app.post('/download', function (req, res) {
   var subset_rows = []
-  fs.readFile(pathUtils.resolve(appDir, 'data/data.csv'), function (err, data) {
-    var subset_rows = []
-    var read_results = Papa.parse(data.toString('utf-8'), {
-      dynamicTyping: true,
-    })
-    var all_rows = read_results['data']
-    subset_rows.push(all_rows.shift()) // add headers
-    req.body.idxs.split('').forEach(function (el, ind) {
-      if (el == '1') {
-        subset_rows.push(all_rows[ind])
-      }
-    })
-    res.contentType('text/plain')
-    res.set('Content-disposition', 'attachment; filename="dramaticpenguin.csv"')
-    res.send(Papa.unparse(subset_rows, { delimiter: '\t' }))
-  })
+  fs.readFile(
+    pathUtils.resolve(appDir, 'data/data_for_download.csv'),
+    function (err, data) {
+      var subset_rows = []
+      var read_results = Papa.parse(data.toString('utf-8'), {
+        dynamicTyping: true,
+      })
+      var all_rows = read_results['data']
+      subset_rows.push(all_rows.shift()) // add headers
+      req.body.idxs.split('').forEach(function (el, ind) {
+        if (el == '1') {
+          subset_rows.push(all_rows[ind])
+        }
+      })
+      res.contentType('text/plain')
+      res.set(
+        'Content-disposition',
+        'attachment; filename="dramaticpenguin.csv"'
+      )
+      res.send(Papa.unparse(subset_rows, { delimiter: '\t' }))
+    }
+  )
 })
 app.use(express.static('static'))
 http.createServer(app).listen(PORT, function () {
